786a12557fa01f930ae6fdccb0e5d5ee35789926
Teekay <tkmaile96@gmail.com>
2024-11-17 09:30:36 +0200

    Added Header component with image and title

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+            src="./images/troll-images.jpg"
+            className="header-image"
+            />
+            <h2 className="header-title">Meme Generator</h2>
+        </header>
+    )
+}
+
