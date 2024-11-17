be46c5842084af80ee9fc338dee5dce863a73118
Teekay <tkmaile96@gmail.com>
2024-11-17 10:10:51 +0200

    Added form inputs for top and bottom text, a button to get a new meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -31,4 +31,40 @@ export default function Meme() {
             randomImage: url // Update only the randomImage property
         }))
     }
+
+    return (
+        <main>
+            <div className="form">
+                <input 
+                type="text"
+                placeholder="Top text"
+                className='form-input'
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}
+                />
+          
+                <input 
+                type="text"
+                placeholder="Bottom text"
+                className='form-input'
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
+                />
+                <button
+                className="form-button"
+                onClick={getMemeImage}
+                >
+                     Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme-image" />
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
+
+        </main>
+    )
 }
